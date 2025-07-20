3ceea9f5c03ed335d4967bf724b12fefcf6e462f
Dena <denasatwikaa@gmail.com>
2025-07-20 19:12:45 +0800

    Fix: Inject environment variables correctly with esbuild

--- js/app/components/supabaseClient.js
+++ js/app/components/supabaseClient.js
@@ -1,5 +1,4 @@
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Membuat client Supabase
 export const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
